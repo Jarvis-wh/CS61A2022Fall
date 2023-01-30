d6497054b35dcc49b896085bf988716e39d2adca
Jarvis-wh <1193966482@qq.com>
2023-01-30 22:49:26 +0800

    riffle shuffle

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -130,4 +130,6 @@ def riffle(deck):
     [0, 10, 1, 11, 2, 12, 3, 13, 4, 14, 5, 15, 6, 16, 7, 17, 8, 18, 9, 19]
     """
     "*** YOUR CODE HERE ***"
-    return _______
+    # 就是一个指示要不要加M的过程
+    return [deck[i // 2 + (i % 2)*(len(deck) // 2)] for i in range(len(deck))]
+    
